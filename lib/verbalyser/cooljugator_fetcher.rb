0834026fb8f7b13d7ce91377c21c7f26f6aaef3e
clockworkpc <clockworkpc@gmail.com>
2017-04-10 13:31:26 +1000

    Second Rspec passed

--- lib/verbalyser/cooljugator_fetcher.rb
+++ lib/verbalyser/cooljugator_fetcher.rb
@@ -9,6 +9,7 @@ module Verbalyser
       @output_folder = "output/"
       @raw_html_output_filename = "#{@output_folder}raw_output.html"
       @nokogiri_output_filename = "#{@output_folder}nokogiri_output.txt"
+      @nokogiri_text_extract_full_raw = "#{@output_folder}nokogiri_text_extract_full_raw.txt"
     end
 
     def fetch_raw_html
@@ -23,12 +24,37 @@ module Verbalyser
       raw_html_completion = File.readlines(File.open(@raw_html_output_filename))
     end
 
-    def read_raw_html
-      puts "Here is the output\n"
-      print File.readlines(@raw_html_output_filename)
+    def parse_raw_html_with_nokogiri
+      raw_data = File.open(@raw_html_output_filename, "r")
+      parsed_content = Nokogiri::HTML.parse(raw_data)
+      output_nokogiri = File.open(@nokogiri_output_filename, "w")
+      output_nokogiri.write(parsed_content)
+
+      parsed_content_file = File.readlines(@nokogiri_output_filename)
+
+      # nokogiri_parsed_content = Fil@nokogiri_output_filename
+      #
+      # demo_parsed_content = File.readlines(File.open("spec/fixtures/demo_nokogiri_content.txt", "r"))
+
+      # puts refined_content
+
+      # ng_parsed = File.open("spec/fixtures/demo_nokogiri_content_parsed.txt", "w")
+
+      # line_items = parsed_content.css("li")
+      #
+      # line_items.each do |line|
+      #   puts line.text
+      #   ng_parsed.write("#{line.text}\n")
+      # end
+
+
+
     end
 
+    def nokogiri_extract_raw_text
 
+      nokogiri_extract_raw_text_demo = File.readlines(File.open("spec/fixtures/demo_nokogiri_text_extract_raw.txt"))
+    end
 
   end
 end
--- spec/verbalyser/cooljugater_fetcher_spec.rb
+++ spec/verbalyser/cooljugater_fetcher_spec.rb
@@ -4,11 +4,29 @@ describe Verbalyser::CooljugatorFetcher do
   let (:raw_html_output_file) { File.open("output/raw_output.html", "r") }
   let (:raw_html_output) { File.readlines(raw_html_output_file) }
 
+  # let (:parsed_nokogiri_content) { File.readlines(File.open("spec/fixtures/demo_nokogiri_content.txt", "r")) }
+  #
+  # let (:nokogiri_text_extract_raw_demo) { File.readlines(File.open("spec/fixtures/demo_nokogiri_text_extract_raw.txt", "r")) }
+  let (:parsed_nokogiri_content) { File.readlines(File.open("output/nokogiri_output.txt", "r")) }
+
   subject { Verbalyser::CooljugatorFetcher.new }
+
   context "uses Cooljutor's complete list of Lithuanian verbs" do
     it "fetches raw data from Cooljugator" do
-      # expect(subject.fetch_raw_html).to eq(File.readlines(File.open("output/raw_output.html", "r")))
       expect(subject.fetch_raw_html).to eq(raw_html_output)
     end
+  end
+
+  context "has retrieved Conjugator's full list of Lithuanian verbs," do
+
+    it "parses the raw data with Nokogiri" do
+      # Readlines turns the Nokogiri document into an array
+      expect(subject.parse_raw_html_with_nokogiri).to eq(parsed_nokogiri_content.to_a)
+    end
+
+    # it "extracts the text from the parsed Nokogiri HTML document" do
+    #   expect(subject.nokogiri_extract_raw_text).to eq(nokogiri_text_extract_raw_demo)
+    #
+    # end
   end
 end
