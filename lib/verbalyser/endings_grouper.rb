8453386a8a2f923a6beae17870db44e925cc6ffa
clockworkpc <clockworkpc@gmail.com>
2017-04-17 00:41:21 +1000

    All tests pass, just need to write the files

--- lib/verbalyser/endings_grouper.rb
+++ lib/verbalyser/endings_grouper.rb
@@ -13,23 +13,24 @@ module Verbalyser
 
       @lithuanian_vowels = %w[a ã à á ą ą̃ ą̀ ą́ e ẽ è é ę ę̃ ę̀ ę́ ė ė̃  ė̀  ė́  i ĩ ì í į̃  į̀ į į y ỹ ỳ ý o õ ò ó ũ ù ú u ų ų̃ ų̀ ų́ ū ū̃  ū̀  ū́]
 
-    end
+      def identify_whether_verb_is_reflexive(infinitive_verb)
+        if infinitive_verb[-2, 2] == "ti"
+          @reflexivity = false
+          @stripped_verb = infinitive_verb[0...-2]
+        elsif infinitive_verb[-3, 3] == "tis"
+          @reflexivity = true
+          @stripped_verb = infinitive_verb[0...-3]
+        end
 
-    def identify_whether_verb_is_reflexive(infinitive_verb)
-
-      if infinitive_verb[-2, 2] == "ti"
-        @reflexivity = "not_reflexive"
-      elsif infinitive_verb[-3, 3] == "tis"
-        @reflexivity = "reflexive"
+        verb_reflexivity = @reflexivity
       end
-
     end
 
-    def find_longest_matching_lemma(verb)
+
+    def check_for_lemma_suffix(stripped_verb)
 
       matching_stem_array = Array.new
-
-      stems = (0..verb.size - 1).each_with_object([]) { |i, subs| subs << verb[0..i] }
+      stems = (0..stripped_verb.size - 1).each_with_object([]) { |i, subs| subs << stripped_verb[0..i] }
 
       stems.each do |stem|
         @lemma_database.each do |lemma|
@@ -40,106 +41,239 @@ module Verbalyser
       end
 
       @matching_lemma = matching_stem_array.max
-
-    end
-
-    def isolate_lemma_suffix(infinitive_verb)
-
-      find_longest_matching_lemma(infinitive_verb)
-
-      if infinitive_verb[-2, 2] == "ti"
-        stripped_standard_infinitive_verb = infinitive_verb[0...-2]
-        stem_ending = stripped_standard_infinitive_verb[@matching_lemma.length..-1]
-
-        if stem_ending.length > 0
-          @nugget = stem_ending
-          puts "#{infinitive_verb} Standard nugget: #{@nugget}"
-        else
-          @stem = stripped_standard_infinitive_verb
-          puts "#{infinitive_verb} stem: #{@stem}"
-        end
-
-      elsif infinitive_verb[-3, 3] == "tis"
-        stripped_reflexive_infinitive_verb = infinitive_verb[0...-3]
-        stem_ending = stripped_reflexive_infinitive_verb[@matching_lemma.length..-1]
-
-        if stem_ending.length > 0
-          @nugget = stem_ending
-          puts "#{infinitive_verb} Reflexive nugget: #{@nugget}"
-        else
-          @stem = stripped_reflexive_infinitive_verb
-          puts "#{infinitive_verb} Reflexive stem: #{@stem}"
-        end
-
-      else
-        puts "This might not be an infinitive verb: #{verb}"
-      end
-
-      puts "This is the nugget #{@nugget} for #{infinitive_verb}"
-
-    end
-
-    def classify_verb_by_forms(infinitive_verb)
-
-      isolate_lemma_suffix(infinitive_verb)
-
-      puts "infinitive_verb: #{infinitive_verb}"
+      @nugget = stripped_verb[@matching_lemma.length..-1]
 
       if @nugget.length > 0
-        puts  "This is the nugget: #{@nugget}"
+        nugget_found = true
+      elsif @nugget.length == 0
+        nugget_found = false
       end
 
+      @lemma_suffix_found = nugget_found
+    end
+
+    def create_classificatory_file_name(infinitive_verb)
+      identify_whether_verb_is_reflexive(infinitive_verb)
+      check_for_lemma_suffix(@stripped_verb)
+
+      @verb_file_name = ""
+
       @verb_database.each do |line|
+
         if line.include?(infinitive_verb)
 
           verb_array = line.strip.split(",")
-          puts "first item: #{verb_array[0]}"
-          puts "second item: #{verb_array[1]}"
-          puts "third item: #{verb_array[2]}"
 
-          @verb_file_name = ""
-
-          verb_array.each do |main_form_verb|
-            puts "This is the main form of the verb: #{main_form_verb}"
-
-            stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
-
-            first_vowel_of_stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́]/]
-
-            puts "This is the main_form_verb starting from '#{first_vowel_of_stem_extract}': #{stem_extract}"
-
-            if @verb_file_name == ""
-              @verb_file_name = stem_extract
-            else
-              @verb_file_name = @verb_file_name + "_" + stem_extract
-            end
+          infinitive_form = verb_array[0].strip
+          present3 = verb_array[1].strip
+          past3 = verb_array[2].strip
 
 
+          if @reflexivity == false && @lemma_suffix_found == true
 
-            # puts "This is the verb_file_name: #{@verb_file_name}"
+            part1 = infinitive_form[-(@nugget.length + "ti".length), (@nugget.length + "ti".length)]
+            part2 = present3[@matching_lemma.length..-1]
+            part3 = past3[@matching_lemma.length..-1]
+
+            @file_name = "#{part1}_#{part2}_#{part3}"
+
+          elsif @reflexivity == true && @lemma_suffix_found == true
+
+            part1 = infinitive_form[-(@nugget.length + "tis".length), (@nugget.length + "tis".length)]
+            part2 = present3[@matching_lemma.length..-1]
+            part3 = past3[@matching_lemma.length..-1]
+
+            @file_name = "#{part1}_#{part2}_#{part3}"
+
+          elsif @reflexivity == false && @lemma_suffix_found == false
+
+            part1 = infinitive_form[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+            part2 = present3[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+            part3 = past3[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+
+            @file_name = "#{part1}_#{part2}_#{part3}"
+
+          elsif @reflexivity == true && @lemma_suffix_found == false
+
+            part1 = infinitive_form[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+            part2 = present3[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+            part3 = past3[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+
+            @file_name = "#{part1}_#{part2}_#{part3}"
 
           end
-          puts "this is the verb file name: #{@verb_file_name}"
 
-          @verb_group_output_filename = @output_folder + @verb_file_name
-          verb_group_output_file = File.open(@verb_group_output_filename, "a")
-          verb_group_output_file.write(line)
-          verb_group_output_file.close
 
         end
       end
 
-      x = @verb_file_name
+      file_name = @file_name
+
 
     end
-
-    def write_verb_forms_to_group_file(infinitive_verb)
-      classify_verb_by_forms(infinitive_verb)
-
-      puts "This is the prospective filename: #{@verb_file_name}"
-    end
-
 
 
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #
+      #       verb_array.each do |main_form_verb|
+      #         puts "This is the main form of the verb: #{main_form_verb}"
+      #
+      #         stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+      #
+      #         first_vowel_of_stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́]/]
+      #
+      #         puts "This is the main_form_verb starting from '#{first_vowel_of_stem_extract}': #{stem_extract}"
+      #
+      #         if @verb_file_name == ""
+      #           @verb_file_name = stem_extract
+      #         else
+      #           @verb_file_name = @verb_file_name + "_" + stem_extract
+      #         end
+      #       end
+      #     end
+      #   end
+        #file name = uoti_uoja_avo
+
+      # elsif @lemma_suffix_found == false
+      #   puts "no lemma suffix found: #{infinitive_verb}"
+      #   # File name = erti_ęra_ẽro
+      # end
+
+    #   puts "This is the file name: #{@verb_file_name}"
+    #
+    #
+    # end
+
+
+    ###
+
+  #   def find_longest_matching_lemma(verb)
+  #
+  #     matching_stem_array = Array.new
+  #
+  #     stems = (0..verb.size - 1).each_with_object([]) { |i, subs| subs << verb[0..i] }
+  #
+  #     stems.each do |stem|
+  #       @lemma_database.each do |lemma|
+  #         if lemma[0] == stem[0] && lemma.strip == stem.strip
+  #           matching_stem_array.push(stem.strip)
+  #         end
+  #       end
+  #     end
+  #
+  #     @matching_lemma = matching_stem_array.max
+  #
+  #   end
+  #
+  #   def isolate_lemma_suffix(infinitive_verb)
+  #
+  #     find_longest_matching_lemma(infinitive_verb)
+  #
+  #     if infinitive_verb[-2, 2] == "ti"
+  #       stripped_standard_infinitive_verb = infinitive_verb[0...-2]
+  #       stem_ending = stripped_standard_infinitive_verb[@matching_lemma.length..-1]
+  #
+  #       if stem_ending.length > 0
+  #         @nugget = stem_ending
+  #         puts "#{infinitive_verb} Standard nugget: #{@nugget}"
+  #       else
+  #         @stem = stripped_standard_infinitive_verb
+  #         puts "#{infinitive_verb} stem: #{@stem}"
+  #       end
+  #
+  #     elsif infinitive_verb[-3, 3] == "tis"
+  #       stripped_reflexive_infinitive_verb = infinitive_verb[0...-3]
+  #       stem_ending = stripped_reflexive_infinitive_verb[@matching_lemma.length..-1]
+  #
+  #       if stem_ending.length > 0
+  #         @nugget = stem_ending
+  #         puts "#{infinitive_verb} Reflexive nugget: #{@nugget}"
+  #       else
+  #         @stem = stripped_reflexive_infinitive_verb
+  #         puts "#{infinitive_verb} Reflexive stem: #{@stem}"
+  #       end
+  #
+  #     else
+  #       puts "This might not be an infinitive verb: #{verb}"
+  #     end
+  #
+  #     puts "This is the nugget #{@nugget} for #{infinitive_verb}"
+  #
+  #   end
+  #
+  #   def classify_verb_by_forms(infinitive_verb)
+  #
+  #     isolate_lemma_suffix(infinitive_verb)
+  #
+  #     puts "infinitive_verb: #{infinitive_verb}"
+  #
+  #     if @nugget.length > 0
+  #       puts  "This is the nugget: #{@nugget}"
+  #     end
+  #
+  #     @verb_database.each do |line|
+  #       if line.include?(infinitive_verb)
+  #
+  #         verb_array = line.strip.split(",")
+  #         puts "first item: #{verb_array[0]}"
+  #         puts "second item: #{verb_array[1]}"
+  #         puts "third item: #{verb_array[2]}"
+  #
+  #         @verb_file_name = ""
+  #
+  #         verb_array.each do |main_form_verb|
+  #           puts "This is the main form of the verb: #{main_form_verb}"
+  #
+  #           stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́](.*)/]
+  #
+  #           first_vowel_of_stem_extract = main_form_verb[/[aãàáąą̃ą̀ą́eẽeẽèéęę̃ę̀ę́ėė̃ė̀ė́iĩìíį̃į̀įįyỹỳýũùúuųų̃ų̀ų́ūū̃ū̀ū́]/]
+  #
+  #           puts "This is the main_form_verb starting from '#{first_vowel_of_stem_extract}': #{stem_extract}"
+  #
+  #           if @verb_file_name == ""
+  #             @verb_file_name = stem_extract
+  #           else
+  #             @verb_file_name = @verb_file_name + "_" + stem_extract
+  #           end
+  #
+  #
+  #
+  #           # puts "This is the verb_file_name: #{@verb_file_name}"
+  #
+  #         end
+  #         puts "this is the verb file name: #{@verb_file_name}"
+  #
+  #         @verb_group_output_filename = @output_folder + @verb_file_name
+  #         verb_group_output_file = File.open(@verb_group_output_filename, "a")
+  #         verb_group_output_file.write(line)
+  #         verb_group_output_file.close
+  #
+  #       end
+  #     end
+  #
+  #     x = @verb_file_name
+  #
+  #   end
+  #
+  #   def write_verb_forms_to_group_file(infinitive_verb)
+  #     classify_verb_by_forms(infinitive_verb)
+  #
+  #     puts "This is the prospective filename: #{@verb_file_name}"
+  #   end
+  # end
--- spec/verbalyser/endings_grouper_spec.rb
+++ spec/verbalyser/endings_grouper_spec.rb
@@ -7,63 +7,107 @@ describe Verbalyser::EndingsGrouper do
   let (:infinitive_verb_reflexive_with_lemma_suffix) {"darbuotis"}
   let (:infinitive_verb_reflexive_without_lemma_suffix) {"rengtis"}
 
+  let (:infinitive_verb_with_lemma_suffix_stripped) {"aktyvuo"}
+  let (:infinitive_verb_without_lemma_suffix_stripped) {"gver"}
+  let (:infinitive_verb_reflexive_with_lemma_suffix_stripped) {"darbuo"}
+  let (:infinitive_verb_reflexive_without_lemma_suffix_stripped) {"reng"}
+
   let (:isolated_lemma_suffix_standard) {"uo"}
-  let (:infinitive_verb_stem) {"gver"}
+  let (:infinitive_verb_stem_shaved) {"er"}
   let (:isolated_lemma_suffix_reflexive) {"uo"}
-  let (:reflexive_verb_stem) {"reng"}
+  let (:reflexive_verb_stem_shaved) {"eng"}
 
   let (:file_name_conjugated_verb_from_lemma_suffix) {"uoti_uoja_avo"}
   let (:file_name_conjugated_verb_from_stem_extract) {"erti_ęra_ẽro"}
-  let (:file_name_reflexive_verb_with_lemma_suffix) {"uotis_úojasi_ãvosi"}
-  let (:file_name_conjugated_reflexiv) {"engtis_eñgiasi_eñgėsi"}
+  let (:file_name_conjugated_reflexive_verb_with_lemma_suffix) {"uotis_úojasi_ãvosi"}
+  let (:file_name_conjugated_reflexive_verb_stem_extract) {"engtis_eñgiasi_eñgėsi"}
 
 
 
   subject {Verbalyser::EndingsGrouper.new}
 
-  context "Receives a set of verbs with diverse conjugations" do
-
+  context "Diverse conjugations, checking for reflexivity" do
     it "identifies whether aktyvuoti is reflexive" do
-      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_with_lemma_suffix)).to eq("not_reflexive")
+      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_with_lemma_suffix)).to eq(false)
     end
 
     it "identifies whether gverti is reflexive" do
-      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_without_lemma_suffix)).to eq("not_reflexive")
+      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_without_lemma_suffix)).to eq(false)
     end
 
     it "identifies whether darbuotis reflexive" do
-      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_reflexive_with_lemma_suffix)).to eq("reflexive")
+      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_reflexive_with_lemma_suffix)).to eq(true)
     end
-
 
     it "identifies whether rengtis reflexive" do
-      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_reflexive_without_lemma_suffix)).to eq("reflexive")
+      expect(subject.identify_whether_verb_is_reflexive(infinitive_verb_reflexive_without_lemma_suffix)).to eq(true)
+    end
+  end
+
+  context "Reflexive verb, checking for lemma suffix" do
+    it "checks for a lemma suffix in darbuotis" do
+      expect(subject.check_for_lemma_suffix(infinitive_verb_reflexive_with_lemma_suffix_stripped)).to eq(true)
     end
 
-    context "Verb is non-reflexive" do
+    it "checks for a lemma suffix in rengtis" do
+      expect(subject.check_for_lemma_suffix(infinitive_verb_reflexive_without_lemma_suffix_stripped)).to eq(false)
+    end
+  end
 
-      it "checks for a lemma suffix in aktyvuoti" do
-        expect(subject.check_for_lemma_suffix(infinitive_verb_with_lemma_suffix)).to eq("lemma_suffix_found")
-      end
-
-      it "checks for a lemma suffix in gverti" do
-        expect(subject.check_for_lemma_suffix(infinitive_verb_without_lemma_suffix)).to eq("lemma_suffix_found")
-      end
+  context "Non-reflexive verb, checking for lemma suffix" do
+    it "checks for a lemma suffix in aktyvuoti" do
+      expect(subject.check_for_lemma_suffix(infinitive_verb_with_lemma_suffix_stripped)).to eq(true)
     end
 
-    context "Verb is reflexive" do
-      it "checks for a lemma suffix in darbuotis" do
-        expect(subject.check_for_lemma_suffix(infinitive_verb_reflexive_with_lemma_suffix)).to eq("lemma_suffix_found")
-      end
-
-      it "checks for a lemma suffix in rengtis" do
-        expect(subject.check_for_lemma_suffix(infinitive_verb_reflexive_without_lemma_suffix)).to eq("lemma_suffix_found")
-      end
-
+    it "checks for a lemma suffix in gverti" do
+      expect(subject.check_for_lemma_suffix(infinitive_verb_without_lemma_suffix_stripped)).to eq(false)
     end
+  end
+
+  context "Non-reflexive verb with lemma suffix, classify by forms" do
+    it "creates a classificatory file name for aktyvuoti" do
+      expect(subject.create_classificatory_file_name(infinitive_verb_with_lemma_suffix)).to eq(file_name_conjugated_verb_from_lemma_suffix)
+    end
+  end
+
+  context "Non-reflexive verb without lemma suffix, classify by forms" do
+    it "creates a classificatory file name for gverti" do
+      expect(subject.create_classificatory_file_name(infinitive_verb_without_lemma_suffix)).to eq(file_name_conjugated_verb_from_stem_extract)
+    end
+  end
+
+  context "Reflexive verb with lemma suffix, classify by forms" do
+    it "creates a classificatory file name for darbuotis" do
+      expect(subject.create_classificatory_file_name(infinitive_verb_reflexive_with_lemma_suffix)).to eq(file_name_conjugated_reflexive_verb_with_lemma_suffix)
+    end
+  end
+
+  context "Reflexive verb with lemma suffix, classify by forms" do
+    it "creates a classificatory file name for darbuotis" do
+      expect(subject.create_classificatory_file_name(infinitive_verb_reflexive_without_lemma_suffix)).to eq(file_name_conjugated_reflexive_verb_stem_extract)
+    end
+  end
+
+
+  # let (:infinitive_verb_with_lemma_suffix) {"aktyvuoti"}
+  # let (:infinitive_verb_without_lemma_suffix) {"gverti"}
+  # let (:infinitive_verb_reflexive_with_lemma_suffix) {"darbuotis"}
+  # let (:infinitive_verb_reflexive_without_lemma_suffix) {"rengtis"}
 
 
 
+  # let (:file_name_conjugated_verb_from_lemma_suffix) {"uoti_uoja_avo"}
+  # let (:file_name_conjugated_verb_from_stem_extract) {"erti_ęra_ẽro"}
+  # let (:file_name_conjugated_reflexive_verb_with_lemma_suffix) {"uotis_úojasi_ãvosi"}
+  # let (:file_name_conjugated_reflexive_verb_stem_extract) {"engtis_eñgiasi_eñgėsi"}
+
+
+  # context "Reflexive verb with lemma suffix, classify by forms" do
+  #   it "creates a classificatory file name for darbuotis" do
+  #
+  #   end
+  # end
+end
 
 
 
@@ -101,5 +145,3 @@ describe Verbalyser::EndingsGrouper do
     # it "appends all three forms to the classificatory file" do
     #   expect(subject.write_verb_forms_to_group_file(infinitive_verb_without_lemma_suffix)).to eq(["gverti,gvęra,gvẽro"])
     # end
-  end
-end
