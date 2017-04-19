376b67218312cd488ee965669bdec6719e49bc2c
clockworkpc <clockworkpc@gmail.com>
2017-04-19 21:48:44 +1000

    Next bug is an old bug, but probably more easily squashed this time

--- spec/verbalyser/verbalyser_integration_testing_spec.rb
+++ spec/verbalyser/verbalyser_integration_testing_spec.rb
@@ -1,15 +1,16 @@
 require "spec_helper"
+require "remove_accents"
 
 describe Verbalyser::EndingsGrouper do
 
-  let (:adaptuoti) {"uoti_úoja_ãvo"}
-  let (:adaptuotis) {"uotis_úojasi_ãvosi"}
-  let (:badyti) {"yti_o_ė"}
-  let (:rakinti) {"inti_ìna_ìno"}
-  let (:plaktis) {"aktis_ãkasi_ãkėsi"}
-  let (:tamsėti) {"ėti_sė́ja_sė́jo"}
-  let (:adyti) {"yti_o_ė"}
-  let (:akinti) {"inti_na_no"}
+  let (:adaptuoti) {"uoti_úoja_ãvo".removeaccents}
+  let (:adaptuotis) {"uotis_úojasi_ãvosi".removeaccents}
+  let (:badyti) {"yti_o_ė".removeaccents}
+  let (:rakinti) {"inti_ìna_ìno".removeaccents}
+  let (:plaktis) {"aktis_ãkasi_ãkėsi".removeaccents}
+  let (:tamsėti) {"ėti_sė́ja_sė́jo".removeaccents}
+  let (:adyti) {"yti_o_ė".removeaccents}
+  let (:akinti) {"inti_na_no".removeaccents}
 
   subject {Verbalyser::EndingsGrouper.new}
 
@@ -19,17 +20,17 @@ describe Verbalyser::EndingsGrouper do
       expect(subject.create_classificatory_file_name("adaptuoti")).to eq(adaptuoti)
     end
 
-    # it "outputs a file name from an infinitive_verb input" do
-    #   expect(subject.create_classificatory_file_name("adaptuotis")).to eq(adaptuotis)
-    # end
-    #
-    # it "outputs a file name from an infinitive_verb input" do
-    #   expect(subject.create_classificatory_file_name("badyti")).to eq(badyti)
-    # end
-    #
-    # it "outputs a file name from an infinitive_verb input" do
-    #   expect(subject.create_classificatory_file_name("rakinti")).to eq(rakinti)
-    # end
+    it "outputs a file name from an infinitive_verb input" do
+      expect(subject.create_classificatory_file_name("adaptuotis")).to eq(adaptuotis)
+    end
+
+    it "outputs a file name from an infinitive_verb input" do
+      expect(subject.create_classificatory_file_name("badyti")).to eq(badyti)
+    end
+
+    it "outputs a file name from an infinitive_verb input" do
+      expect(subject.create_classificatory_file_name("rakinti")).to eq(rakinti)
+    end
     #
     # it "outputs a file name from an infinitive_verb input" do
     #   expect(subject.create_classificatory_file_name("plaktis")).to eq(plaktis)
